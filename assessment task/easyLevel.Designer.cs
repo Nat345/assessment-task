dc5805f9f67f7c82a455ba1edeb317fcd8b443ed
Nat <Natalie@education.nsw.gov.au>
2022-08-16 12:55:15 +1000

    added all the pics

--- assessment task/easyLevel.Designer.cs
+++ assessment task/easyLevel.Designer.cs
@@ -84,6 +84,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(178, 78);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -93,38 +94,47 @@
             this.pictureBox2.Location = new System.Drawing.Point(348, 78);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox3
             // 
             this.pictureBox3.Location = new System.Drawing.Point(525, 78);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // pictureBox4
             // 
             this.pictureBox4.Location = new System.Drawing.Point(704, 78);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
             // pictureBox5
             // 
             this.pictureBox5.Location = new System.Drawing.Point(874, 78);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
             // 
             // pictureBox6
             // 
             this.pictureBox6.Location = new System.Drawing.Point(874, 235);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 9;
             this.pictureBox6.TabStop = false;
             // 
@@ -133,6 +143,7 @@
             this.pictureBox7.Location = new System.Drawing.Point(704, 235);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox7.TabIndex = 8;
             this.pictureBox7.TabStop = false;
             // 
@@ -141,30 +152,37 @@
             this.pictureBox8.Location = new System.Drawing.Point(525, 235);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox8.TabIndex = 7;
             this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
             // 
             // pictureBox9
             // 
             this.pictureBox9.Location = new System.Drawing.Point(348, 235);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox9.TabIndex = 6;
             this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
             // 
             // pictureBox10
             // 
             this.pictureBox10.Location = new System.Drawing.Point(178, 235);
             this.pictureBox10.Name = "pictureBox10";
             this.pictureBox10.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox10.TabIndex = 5;
             this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.pictureBox10_Click);
             // 
             // pictureBox11
             // 
             this.pictureBox11.Location = new System.Drawing.Point(874, 384);
             this.pictureBox11.Name = "pictureBox11";
             this.pictureBox11.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox11.TabIndex = 14;
             this.pictureBox11.TabStop = false;
             // 
@@ -173,6 +191,7 @@
             this.pictureBox12.Location = new System.Drawing.Point(704, 384);
             this.pictureBox12.Name = "pictureBox12";
             this.pictureBox12.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox12.TabIndex = 13;
             this.pictureBox12.TabStop = false;
             // 
@@ -181,6 +200,7 @@
             this.pictureBox13.Location = new System.Drawing.Point(525, 384);
             this.pictureBox13.Name = "pictureBox13";
             this.pictureBox13.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox13.TabIndex = 12;
             this.pictureBox13.TabStop = false;
             // 
@@ -189,6 +209,7 @@
             this.pictureBox14.Location = new System.Drawing.Point(348, 384);
             this.pictureBox14.Name = "pictureBox14";
             this.pictureBox14.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox14.TabIndex = 11;
             this.pictureBox14.TabStop = false;
             // 
@@ -197,6 +218,7 @@
             this.pictureBox15.Location = new System.Drawing.Point(178, 384);
             this.pictureBox15.Name = "pictureBox15";
             this.pictureBox15.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox15.TabIndex = 10;
             this.pictureBox15.TabStop = false;
             // 
@@ -205,6 +227,7 @@
             this.pictureBox16.Location = new System.Drawing.Point(874, 543);
             this.pictureBox16.Name = "pictureBox16";
             this.pictureBox16.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox16.TabIndex = 19;
             this.pictureBox16.TabStop = false;
             // 
@@ -221,6 +244,7 @@
             this.pictureBox18.Location = new System.Drawing.Point(525, 543);
             this.pictureBox18.Name = "pictureBox18";
             this.pictureBox18.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox18.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox18.TabIndex = 17;
             this.pictureBox18.TabStop = false;
             // 
@@ -229,6 +253,7 @@
             this.pictureBox19.Location = new System.Drawing.Point(348, 543);
             this.pictureBox19.Name = "pictureBox19";
             this.pictureBox19.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox19.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox19.TabIndex = 16;
             this.pictureBox19.TabStop = false;
             // 
@@ -237,6 +262,7 @@
             this.pictureBox20.Location = new System.Drawing.Point(178, 543);
             this.pictureBox20.Name = "pictureBox20";
             this.pictureBox20.Size = new System.Drawing.Size(122, 107);
+            this.pictureBox20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox20.TabIndex = 15;
             this.pictureBox20.TabStop = false;
             // 
--- assessment task/easyLevel.cs
+++ assessment task/easyLevel.cs
@@ -13,12 +13,31 @@ namespace assessment_task
     public partial class easyLevel : Form
     {
         //the numbers that are assigned to the pictures on easy level
-        List<int> picNumbers = new List<int> () { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10};
+        List<PictureBox> picNumbers = new List<PictureBox>();
         
-
         public easyLevel()
         {
             InitializeComponent();
+            picNumbers.Add(pictureBox1);
+            picNumbers.Add(pictureBox2);
+            picNumbers.Add(pictureBox3);
+            picNumbers.Add(pictureBox4);
+            picNumbers.Add(pictureBox5);
+            picNumbers.Add(pictureBox6);
+            picNumbers.Add(pictureBox7);
+            picNumbers.Add(pictureBox8);
+            picNumbers.Add(pictureBox9);
+            picNumbers.Add(pictureBox10);
+            picNumbers.Add(pictureBox11);
+            picNumbers.Add(pictureBox12);
+            picNumbers.Add(pictureBox13);
+            picNumbers.Add(pictureBox14);
+            picNumbers.Add(pictureBox15);
+            picNumbers.Add(pictureBox16);
+            picNumbers.Add(pictureBox17);
+            picNumbers.Add(pictureBox18);
+            picNumbers.Add(pictureBox19);
+            picNumbers.Add(pictureBox20);
         }
 
         private void TimerEvent(object sender, EventArgs e)
@@ -49,11 +68,45 @@ namespace assessment_task
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             //I'm trying to make a click event where the picture appears when its clicked 
-            //pictureBox1.Image = Properties.Resources.;
-            
-            
+            pictureBox1.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number1.png");
 
-            
         }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number2.png");
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number3.png");
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number4.png");
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number5.png");
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            pictureBox10.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number10.png");
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            pictureBox9.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number9.png");
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            pictureBox8.Image = Image.FromFile(@"C:\Users\Natalie\source\repos\New folder\assessment task\number8.png");
+        }
+
+        
     }
 }
